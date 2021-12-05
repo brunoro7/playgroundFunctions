43884bb1293a4b9ae1941adad66a8388132e5745
brunoro7 <bruno.ro7c@gmail.com>
2021-12-04 22:30:15 -0300

    entregando desafio9.

--- src/challenges.js
+++ src/challenges.js
@@ -88,31 +88,63 @@ function fizzBuzz(arrayDeNum) {
 function encode(frase) {
 
   let splitedFrase = frase.split("");
+  let modifiedFrase = [];
 
-/**  case "a" = 1
-  case "e" = 2
-  case "i" = 3
-  case "o" = 4
-  case "u" = 5
-*/
+  for(let i=0; i<splitedFrase.length; i++){
 
-  return splitedFrase
-}
-console.log(encode("hi there!"))
-
-function decode(modifiedFrase) {
+    if(splitedFrase[i]==="a") {
+      modifiedFrase.push("1");
+    }
+    else if(splitedFrase[i]==="e") {
+      modifiedFrase.push("2");
+    }
+    else if(splitedFrase[i]==="i") {
+      modifiedFrase.push("3");
+    }
+    else if(splitedFrase[i]==="o") {
+      modifiedFrase.push("4");
+    }
+    else if(splitedFrase[i]==="u") {
+      modifiedFrase.push("5");
+    }
+    else{
+      modifiedFrase.push(splitedFrase[i]);
+    }
+  }
   let joinedFrase = modifiedFrase.join("");
-
-/**  case "1" = "a"
-  case "2" = "e"
-  case "3" = "i"
-  case "4" = "o"
-  case "5" = "u"
-*/
 
   return joinedFrase
 }
-console.log(decode(encode("hi there!")))
+
+function decode(fraseCod) {
+  let splitedFraseCod = fraseCod.split("");
+  let modifiedFraseCod = [];
+
+  for(let i=0; i<splitedFraseCod.length; i++){
+
+    if(splitedFraseCod[i]==="1") {
+      modifiedFraseCod.push("a");
+    }
+    else if(splitedFraseCod[i]==="2") {
+      modifiedFraseCod.push("e");
+    }
+    else if(splitedFraseCod[i]==="3") {
+      modifiedFraseCod.push("i");
+    }
+    else if(splitedFraseCod[i]==="4") {
+      modifiedFraseCod.push("o");
+    }
+    else if(splitedFraseCod[i]==="5") {
+      modifiedFraseCod.push("u");
+    }
+    else{
+      modifiedFraseCod.push(splitedFraseCod[i]);
+    }
+  }
+  let joinedFraseCod = modifiedFraseCod.join("");
+
+  return joinedFraseCod
+}
 
 module.exports = {
   calcArea,
